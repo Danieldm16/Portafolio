1624565a528e98613771befd780531b0498db50f
Juan Daniel Salazar Coronado <46731437+Danieldm16@users.noreply.github.com>
2021-12-17 17:07:07 -0600

    Add files via upload

---
+++ Proyectos web/cursos2021/aspirantes.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Registro de vacunación</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+    </head>
+<body>
+    <div class="container-fluid">
+
+        <h1>Preregistor a cursos de capacitación</h1>
+        <p>Complete los datos requeridos, esta pantalla esta apta para dispositivos móviles</p>
+
+        <form action="#" method="post" name="forma">
+
+            <div class="row">
+
+                <div class="col-25">
+                    <label for="lrfc">R.F.C:</label>
+                </div><!-- aqui cierra col-25-->
+
+                <div class="col-75">
+                    <input type="text" name="f_rfc" id="f_rfc" placeholder="Ingrese su RFC">
+                </div><!-- aqui cierra col-75-->
+
+                <div class="nombre">
+                    <label for="lnombre">Nombre:</label>
+                </div><!-- aqui cierra nombre-->
+
+                <div class="inputnombre">
+                    <input type="text" name="f_nombre" id="f_nombre" placeholder="Ingrese su nombre">
+                </div><!-- aqui cierra inputnombre-->
+
+                <div class="paterno">
+                    <label for="lpaterno">Apellido Paterno:</label>
+                </div><!-- aqui cierra paterno-->
+
+                <div class="inputpaterno">
+                    <input type="text" name="f_paterno" id="f_paterno" placeholder="Ingrese su apellido paterno">
+                </div><!-- aqui cierra inputpaterno-->
+
+                <div class="materno">
+                    <label for="lmaterno">Apellido Materno:</label>
+                </div><!-- aqui cierra materno-->
+
+                <div class="inputmaterno">
+                    <input type="text" name="f_materno" id="f_materno" placeholder="Ingrese su apellido paterno">
+                </div><!-- aqui cierra inputmaterno-->
+
+                <div class="empresa">
+                    <label for="lempresa">Empresa donde trabaja:</label>
+                </div><!-- aqui cierra empresa-->
+
+                <div class="inputempresa">
+                    <input type="text" name="f_empresa" id="f_empresa" placeholder="Ingrese el nombre de la empresa">
+                </div><!-- aqui cierra inputempresa-->
+
+                <div class="tel">
+                    <label for="ltel">Ingrese su número de teléfono:</label>
+                </div><!-- aqui cierra tel-->
+
+                <div class="inputtel">
+                    <input type="number" name="f_tel" id="f_tel" placeholder="Ingrese su telefono">
+                </div><!-- aqui cierra inputtel-->
+
+                <div class="email">
+                    <label for="lrfc">Ingrese su correo electrónico:</label>
+                </div><!-- aqui cierra email-->
+
+                <div class="inputemail">
+                    <input type="email" name="f_email" id="f_email" placeholder="Ingrese su correo">
+                </div><!-- aqui cierra inputemail-->
+
+                <div class="submit">
+                    <input type="submit" name="f_submit" id="f_submit" placeholder="Guardar datos">
+                </div><!-- aqui cierra submit-->
+
+            </div><!-- aqui cierra row-->
+        </form><!-- aqui cierra el form-->
+    </div><!-- aqui cierra container-fluid-->
+</body>
+</html>
